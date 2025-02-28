1adfae6a89617485f39608c612500bf1b7f60f60
Anzhela Akhmatava <angela.akhmatova@gmail.com>
2025-02-28 08:55:37 +0100

    feature_3 commit very sofisticated python code

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from tools import add
+
+
 class Person:
     def __init__(self, name, age):
         self.name = name
@@ -6,3 +9,7 @@ class Person:
         return self.name
     
 person_1 = Person("sss", 12)
+person_2 = Person("Mathilde", 14)
+
+personen_avg_age = add(person_1.age, person_2.age) / 2
+print(personen_avg_age)
