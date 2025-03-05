eb3cdf06474abe9c717ae9d6bed2d9830b7c29ee
Anzhela Akhmatava <angela.akhmatova@gmail.com>
2025-03-05 14:28:34 +0100

    modified get_all_tagebuch_input_per_word

--- tagebuch.py
+++ tagebuch.py
@@ -50,6 +50,8 @@ class Tagebuch:
                         #the word is in the file
                         files_with_text.append(file_name)
                         break
+
+        print("serach per words is successful")
         return files_with_text
         
 if __name__ == "__main__":
